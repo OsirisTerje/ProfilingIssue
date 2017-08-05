27e19a7b2823f72641e2cf79b2ad81ddf07d2469
Terje Sandstrom <terje@hermit.no>
2017-08-05 23:09:46 +0200

    Added mstest project

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ClassLibrary1
+{
+    [TestClass]
+    public class Class1
+    {
+        [TestMethod]
+        public void TestItMsTest()
+        {
+            Assert.IsTrue(true);
+        }
+
+    }
+}
