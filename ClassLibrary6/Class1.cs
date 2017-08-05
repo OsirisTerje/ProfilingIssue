cad309d42d75e76ed6606d7ccf1bf883b048721c
Terje Sandstrom <terje@hermit.no>
2017-08-05 23:03:54 +0200

    Initial code with NUnit

--- ClassLibrary6/Class1.cs
+++ ClassLibrary6/Class1.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
 
 namespace ClassLibrary6
 {
     public class Class1
     {
+        [Test]
+        public void TestIt()
+        {
+            Assert.Pass();
+        }
     }
 }
